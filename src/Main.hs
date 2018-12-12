e5fe88287fe4c3f42b116be828ee324e5cb1501b
Tommy <Tommy>
2018-12-12 13:28:35 -0800

    starting work on FIO, cleaning up and adding comments

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,8 @@ module Main where
 
 import Control.Applicative
 import Control.Monad(liftM, ap)
+import Control.Monad.Cont(ContT, runContT)
+import Control.Monad.Reader(ReaderT, runReaderT, ask)
 import Control.Monad.Trans.Class(lift)
 import Data.IORef
 import Data.Monoid((<>))
@@ -10,6 +12,11 @@ import Data.List(find)
 import Data.String
 
 import Web.Scotty
+
+-- Database API:
+--   CreatePost(Username, Content)
+--   AddFriend(Username, Username)
+--   RemoveFriend(Username, Username)
 
 type Post = String
 type User = String
@@ -19,20 +26,84 @@ data Label =
     Top
   | U User
   | Bot
-  deriving Show
+  deriving (Show, Eq)
 leq :: LatticeState -> Label -> Label -> Bool
 leq s Bot    _      = True
 leq s _      Bot    = False
 leq s _      Top    = True
 leq s Top    _      = False
 leq s (U u1) (U u2) =
+  if u1==u2 then
+    True
+  else
+    let LS edges = s  in
+    find ((u1,u2)==) edges /= Nothing
+join :: LatticeState -> Label -> Label -> Label
+join s Bot    k      = k
+join s k      Bot    = k
+join s _      Top    = Top
+join s Top    _      = Top
+join s (U u1) (U u2) =
   let LS edges = s  in
-  find ((u1,u2)==) edges /= Nothing
+  if u1==u2 then
+    U u1
+  else if find ((u1,u2)==) edges /= Nothing then
+    U u2
+  else if find ((u2,u1)==) edges /= Nothing then
+    U u1
+  else
+    Top
 
 data Fac a where
   Raw     ::                         a      -> Fac a
   Fac     :: Label -> Fac a ->     Fac a    -> Fac a
   BindFac ::          Fac a -> (a -> Fac b) -> Fac b
+
+data FIORef a =
+  FIORef (IORef (Fac a))
+
+data FIO a where
+  Return :: a -> FIO a
+  BindFIO :: FIO a -> (a -> FIO b) -> FIO b
+  Swap :: Fac (FIO a) -> FIO (Fac a)
+  New :: a -> FIO (FIORef a)
+  Read :: FIORef a -> FIO (Fac a)
+  Write :: FIORef a -> Fac a -> FIO ()
+
+type PC = (Label, [Label])
+consistent :: LatticeState -> PC -> Bool
+consistent s (k1, []) = k1 /= Top
+consistent s (k1, k2:ks) = not (leq s k2 k1) && consistent s (k1, ks)
+add_pc :: LatticeState -> PC -> Label -> PC
+add_pc s (k, ks) k' = (join s k k', ks)
+subtract_pc :: LatticeState -> PC -> Label -> PC
+subtract_pc _ (k, ks) k' = (k, k':ks)
+
+type M = ContT () (ReaderT PC IO)
+
+run :: LatticeState -> FIO a -> M a
+run s (Return x) =
+  return x
+run s (BindFIO x f) = do
+  y <- run s x
+  run s (f y)
+run s (Swap (Raw ia)) = do
+  a <- run s ia
+  return (Raw a)
+run s (Swap (BindFac (Raw b) f)) =
+  run s (Swap (f b))
+run s (Swap (BindFac (Fac k fb1 fb2) f)) =
+  run s (Swap (Fac k (BindFac fb1 f) (BindFac fb2 f)))
+run s (Swap (BindFac (BindFac c g) f)) =
+  run s (Swap (BindFac c (\x -> BindFac (g x) f)))
+run s (Swap (Fac k fia1 fia2)) = do
+  pc <- ask
+  if   not (consistent s (subtract_pc s pc k))   then
+    run s (Swap fia1)
+  else if   not (consistent s (add_pc s pc k))   then
+    run s (Swap fia2)
+  else
+    error (show pc ++ show k)
 
 instance Functor Fac where
   fmap = liftM
@@ -43,7 +114,17 @@ instance Monad Fac where
   return = Raw
   (>>=) = BindFac
 
+project :: LatticeState -> Label -> Fac a -> IO a
+project s k fa = do
+  temp <- newIORef undefined
+  runReaderT (runContT (run s (Swap $ BindFac fa (\a -> Raw (Return a)))) (\(Raw a) -> do
+      lift $ writeIORef temp a
+      return ()
+    )) (k, [])
+  readIORef temp
+
 -- Unsafe function
+{-
 project :: LatticeState -> Label -> Fac a -> a
 project lattice k1 = g where
   g :: Fac a -> a
@@ -56,6 +137,7 @@ project lattice k1 = g where
       g fa2
   g (BindFac fb1 c) =
     g (c (g fb1))
+-}
 
 -------------------------------
 --  Above is in TCB          --
@@ -122,10 +204,11 @@ display_main_page username d =
 --  Below is in TCB          --
 -------------------------------
 
-{-
+-- Faceted section --
+
 type FDatabase = (Fac (FList Post), LatticeState)
 
-main = do  --IO
+main_faceted = do  --IO
   database <- newIORef ((Raw Nil, LS []) :: FDatabase)
   scotty 3000 $ do  --Scotty
     post "/add-friend" $ do  --ScottyIO
@@ -152,9 +235,12 @@ main = do  --IO
     get "/read-all-posts/:username" $ do  --ScottyIO
       username <- param "username"
       (flist, lattice) <- lift $ readIORef database
-      let posts = project lattice (U username) (flatten flist)
+      posts <- lift $ project lattice (U username) (flatten flist)
       display_main_page username posts
--}
+
+----------------------------
+
+-- Non-faceted section --
 
 type Database = ([(Label, Post)], LatticeState)
 
@@ -170,7 +256,7 @@ filter_database username (posts, lattice) =
       else
         filtered_tail
 
-main = do  --IO
+main_nonfaceted = do  --IO
   database <- newIORef ([], LS [])
   scotty 3000 $ do  --Scotty
     post "/add-friend" $ do  --ScottyIO
@@ -199,3 +285,5 @@ main = do  --IO
       db <- lift $ readIORef database
       let posts = filter_database username db
       display_main_page username posts
+
+main = main_faceted
